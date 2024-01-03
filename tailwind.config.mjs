8e31da8ea01abc9750f0bf72b6b8636d56db626d
Will <30413278+wcrum@users.noreply.github.com>
2024-01-02 20:26:00 -0700

    Initial Commit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,11 +4,23 @@ module.exports = {
 		'./node_modules/flowbite/**/*.js'
 	],
 	theme: {
+		fontFamily: {
+			'espiritu': ['Espiritu'],
+			'nexarustscript': ['NexaRustScript'],
+			'rodfat': ['RodFat'],
+		},
 		extend: {
 			colors: {
 				'kava-dark': '#161618',
 				'kava-gold': '#ac8448',
+				'kava-gold-dark': '#7d6034',
+				'kava-green': '#243a28',
+				'kava-brown': '#6b3920',
+				'kava-light': '#ffffeb'
 			  },
+			backgroundImage: {
+				'gkl-pattern': 'url("/gkl-pattern.svg")'
+			}
 		},
 	},
 	plugins: [
